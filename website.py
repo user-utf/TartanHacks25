6633ec4c49bf494b7cdaa0ed686a5ef49d2bbbeb
user-utf <alt35308@gmail.com>
2025-02-08 01:02:22 -0500

    added department button functionality

--- website.py
+++ website.py
@@ -10,9 +10,14 @@ def index():
     return render_template("index.html",img_url="https://upload.wikimedia.org/wikipedia/commons/c/c0/Douglas_adams_portrait_cropped.jpg")
 
 @app.route('/random')
-def hello():
+def random():
     data = collection.get_image(collection.random_item())
-    print(data)
+    return render_template("index.html",img_url=data[0], title = data[1], artist = data[2], department = data[3])
+
+@app.route('/department<dep>')
+def department(dep):
+    print(dep)
+    data = collection.random_image_from_dep(dep)
     return render_template("index.html",img_url=data[0], title = data[1], artist = data[2], department = data[3])
 
 if __name__ == '__main__':
